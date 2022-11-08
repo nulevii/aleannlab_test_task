01087c7fb20352353b2de999c20022291244c133
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-08 12:33:15 +0200

    Bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import * as React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { useFetch } from './utilities/useFetch'
 
 import { API_LINK } from './utilities/constants'
-
 import ScrollToTop from './scrollToTop'
+
 import Loader from './components/loader/loader'
 import JobList from './components/job-list'
-import DetailedJob from './components/detailed-job'
+const DetailedJob = React.lazy(async () => await import('./components/detailed-job'))
 
 function App () {
   const { loading, data: jobs } = useFetch(API_LINK)
   if (loading) {
-    return <Loader/>
+    return <Loader />
   }
 
   return (
@@ -23,7 +23,14 @@ function App () {
           <Route path="" element={<Navigate to="/job_list/1" />}></Route>
           <Route path=":id" element={<JobList jobs={jobs} />}></Route>
         </Route>
-        <Route path="/job/:id" element={<DetailedJob jobs={jobs} />}></Route>
+        <Route
+          path="/job/:id"
+          element={
+            <React.Suspense fallback={<><Loader/></>}>
+              <DetailedJob jobs={jobs} />
+            </React.Suspense>
+          }
+        ></Route>
         <Route path="*" element={<Navigate to="/job_list/1" />}></Route>
       </Routes>
     </>
